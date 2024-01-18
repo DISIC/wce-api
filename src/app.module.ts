d724ca9814233b238e077cfa5633fcb053c3dd48
youssefElMkhantar <youssef.mkht@gmail.com>
2024-01-18 17:50:47 +0100

    ajout des quelques modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { ConferenceModule } from './conference/conference.module';
 
 @Module({
-  imports: [],
+  imports: [AuthenticationModule, ConferenceModule],
   controllers: [AppController],
   providers: [AppService],
 })
