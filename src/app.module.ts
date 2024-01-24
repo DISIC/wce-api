07dde0dbdcc3ea0daf0d54dfb5ed5f0479720c9e
youssefElMkhantar <youssef.mkht@gmail.com>
2024-01-24 08:09:44 +0100

    configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { ConferenceModule } from './conference/conference.module';
+import { StatsModule } from './stats/stats.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthenticationModule, ConferenceModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          uri: configService.get('MONGO_URI'),
+        };
+      },
+      inject: [ConfigService],
+    }),
+    AuthenticationModule,
+    ConferenceModule,
+    StatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
